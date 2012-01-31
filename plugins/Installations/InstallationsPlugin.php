5e4ad0c13c9bc4840ef3c7a84a063b487529c25b
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-01-31 18:28:02 -0500

    Begins adding Europeana properties to RecordRelations

--- plugins/Installations/InstallationsPlugin.php
+++ plugins/Installations/InstallationsPlugin.php
@@ -3,26 +3,35 @@ define('INSTALLATIONS_PLUGIN_DIR', dirname(__FILE__));
 require_once PLUGIN_DIR . '/RecordRelations/includes/models/RelatableRecord.php';
 
 if (class_exists('Omeka_Plugin_Abstract')) {
-    
+
     class InstallationsPlugin extends Omeka_Plugin_Abstract
     {
-        
+
         protected $_hooks = array(
         	'install',
         	'uninstall',
         	'installation_browse_sql',
-        	'public_theme_header'
-        
+        	'public_theme_header',
+
+
         );
-        protected $_filters = array();
+        protected $_filters = array(
+        	'admin_navigation_main'
+
+        );
         protected $_options = null;
-            
+
         public function hookPublicThemeHeader()
         {
             queue_css('installations');
         }
-                
-        
+
+        public function filterAdminNavigationMain($tabs)
+        {
+            $tabs['Installations'] = uri('installations/index/browse');
+            return $tabs;
+        }
+
         public function hookInstall()
         {
             $db = get_db();
@@ -43,7 +52,7 @@ if (class_exists('Omeka_Plugin_Abstract')) {
               `author_info` text NULL,
               PRIMARY KEY (`id`)
             ) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci
-            
+
             ";
             $db->query($sql);
             //InstallationCollection table
@@ -58,9 +67,9 @@ if (class_exists('Omeka_Plugin_Abstract')) {
               PRIMARY KEY (`id`)
             ) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci
             ";
-                    
+
             $db->query($sql);
-            
+
             //InstallationExhibit table
             $sql = "
             CREATE TABLE IF NOT EXISTS `$db->InstallationContextExhibit` (
@@ -73,9 +82,9 @@ if (class_exists('Omeka_Plugin_Abstract')) {
               PRIMARY KEY (`id`)
             ) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci
             ";
-            
+
             $db->query($sql);
-            
+
             $sql = "
             CREATE TABLE IF NOT EXISTS `$db->InstallationContextExhibitSection` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -90,7 +99,7 @@ if (class_exists('Omeka_Plugin_Abstract')) {
             ";
 
             $db->query($sql);
-            
+
             $sql = "
             CREATE TABLE IF NOT EXISTS `$db->InstallationContextExhibitSectionPage` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -103,8 +112,8 @@ if (class_exists('Omeka_Plugin_Abstract')) {
               PRIMARY KEY (`id`)
             ) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci
             ";
-            
-            
+
+
             $db->query($sql);
             //InstallationItem table
             $sql = "
@@ -119,7 +128,7 @@ if (class_exists('Omeka_Plugin_Abstract')) {
             ) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_unicode_ci
             ";
             $db->query($sql);
-      
+
             $sql = "
             CREATE TABLE IF NOT EXISTS `$db->InstallationToken` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -130,9 +139,9 @@ if (class_exists('Omeka_Plugin_Abstract')) {
               UNIQUE KEY `installation_ids` (`installation_id`)
             ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
             ";
-                    
+
             $db->query($sql);
-            
+
             $prop = get_db()->getTable('RecordRelationsProperty')->findByVocabAndPropertyName(SIOC, 'has_container');
             if(empty($prop)) {
                 $propData = array(
@@ -148,13 +157,47 @@ if (class_exists('Omeka_Plugin_Abstract')) {
                 );
                 record_relations_install_properties(array($propData));
             }
-            
+
+			//put in Europeana properties
+			$europeanaProps = array(
+			      array(
+			            'name' => 'Europeana',
+			            'description' => 'Europeana relations',
+			            'namespace_prefix' => 'europeana',
+			            'namespace_uri' => EUROPEANA,
+			            'properties' => array(
+			                array(
+			                    'local_part' => 'isShownBy',
+			                    'label' => 'is shown by',
+			                    'description' => ''
+			                ),
+			                array(
+			                	'local_part' => 'isDisplayedBy',
+			                	'label' => 'is displayed by',
+			                	'description' => ''
+			                ),
+			                array(
+			                	'local_part' => 'provider',
+			                	'label' => 'provider',
+			                	'description' => ''
+			                ),
+			                array(
+			                	'local_part' => 'dataProvider',
+			                	'label' => 'data provider',
+			                	'description' => ''
+			                ),
+			            )
+			        )
+
+			  );
+			record_relations_install_properties($europeanaProps);
+
             $blocks = unserialize(get_option('blocks'));
             $blocks[] = 'CommonsOriginalInfoBlock';
             $blocks[] = 'CommonsLicenseInfoBlock';
             set_option('blocks', serialize($blocks));
         }
-        
+
         public function hookUninstall()
         {
 
@@ -168,7 +211,7 @@ if (class_exists('Omeka_Plugin_Abstract')) {
             		`$db->InstallationItem`,
             		`$db->InstallationToken` ;
             ";
-          
+
             $db->exec($sql);
             $blocks = unserialize(get_option('blocks'));
             $blocks = array_flip($blocks);
@@ -177,7 +220,7 @@ if (class_exists('Omeka_Plugin_Abstract')) {
             $blocks = array_flip($blocks);
             set_option('blocks', serialize($blocks));
         }
-        
+
         public function hookInstallationBrowseSql($select, $params)
         {
             if(isset($_GET['unapproved']) && $_GET['unapproved'] == true) {
@@ -185,7 +228,7 @@ if (class_exists('Omeka_Plugin_Abstract')) {
             }
             return $select;
         }
-  
-        
+
+
     }
 }
